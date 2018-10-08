b97ecd523c76d7b93208c47cd446e9587a4d8fdf
Mateus Gusmão <teumarques14@gmail.com>
2018-10-08 20:41:15 -0300

    Atividade TA - Jinq -  05/10/2018

---
+++ src/main/java/br/edu/ifpe/jpa/example/App.java
@@ -0,0 +1,110 @@
+package br.edu.ifpe.jpa.example;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.jinq.jpa.JinqJPAStreamProvider;
+
+import br.edu.ifpe.jpa.example.entities.Blog;
+import br.edu.ifpe.jpa.example.entities.Car;
+
+public class App {
+	
+	static EntityManagerHelper helper = EntityManagerHelper.getInstance();
+	
+	/*public static void main(String[] args) {
+		helper.execute(Car.class, streams -> {
+			streams
+				.where(c -> c.getPrice() >= 3000)
+				.sortedBy(c -> c.getName())
+				.toList()
+				.stream()
+				.forEach(System.out::println);
+		});
+		
+	}*/
+
+	
+	// 1. Imprima na tela todos os blogs que possuem o id maior que 10
+	public void questaoUm() {
+		helper.execute(Blog.class, streams -> {
+			streams
+				.where(b -> b.getIdentifier() > 10)
+				.toList()
+				.stream()
+				.forEach(System.out::println);
+		});
+
+	}
+
+	// 2. Imprima na tela a descrição do blog que possui o nome "dia a dia, bit a bit"
+	public void questaoDois() {
+		helper.execute(Blog.class, streams->{
+			streams.
+			where(b -> b.getName().equals("dia a dia, bit a bit"))
+			.toList()
+			.stream()
+			.forEach(b->System.out.println(b.getDescription()));
+		});
+	}
+
+	// 3. Imprima na tela as decrições dos 5 primeiros blogs criados (considerar o atributo creationDate)
+	public void questaoTres() {
+		helper.execute(Blog.class, streams -> {
+			streams.
+			sortedBy(b->b.getCreationDate())
+			.limit(5)
+			.toList()
+			.stream()
+			.forEach(b->System.out.println(b.getDescription()));
+		});
+	}
+
+	// 4. Imprima na tela o título e conteúdo de todos os posts do blog com título recebido como parâmetro, 
+	//ordenados alfabeticamente pelo título do post
+	public void questaoQuatro(String titulo) {
+		helper.execute(Blog.class, streams -> {
+			streams.
+				.where(b -> b.getPosts().getTitle().equals(titulo))
+				.toList()
+				.getPosts()
+				.stream()
+				.forEach(p -> System.out.println(p.getTitle() + "\n" + p.getContent() + "\n"));
+		});
+	}
+
+	// 5. Imprima na tela o título do último post do blog com título "título"
+	public void questaoCinco(String titulo) {
+		helper.execute(Blog.class, streams -> {
+			streams.
+				.where(b -> b.getName().equals(titulo))
+				.sortedBy(b->b.getPosts().getCreationDate())
+				.limit(1)
+				.toList()
+				.stream()
+				.forEach(p -> p.getPosts().getTitle());
+		});
+
+	}
+
+	// 6. Retorne uma lista com os títulos de todos os posts publicados no blog com título tituloBlog 
+	//entre o período dataInicial e dataFinal.
+	public List<String> questaoSeis(Date dataInicial, Date dataFinal, String tituloBlog) {
+		throw new UnsupportedOperationException();
+	}
+
+	// 7. Imprima na tela a média de posts existentes nos blogs
+	public void questaoSete() {
+		helper.execute(Blog.class, streams -> {
+			streams.
+				.select(b -> b.getPosts())
+				.avg(b -> b.getPosts());
+		});
+
+	}
+	}
+
